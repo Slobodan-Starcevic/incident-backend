6f1fd5235e2d08d90e9e2711d96c4c3280f0bfa3
Slobodan <slobodan9900@outlook.com>
2025-09-03 10:17:28 +0200

    springboot setup

---
+++ src/main/java/com/innovactions/incident/IncidentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.innovactions.incident;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncidentBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/innovactions/incident/IncidentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innovactions.incident;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
